f5d59ae5fefa3d21ad903d5f5b9558d2979ebae6
devansh280904 <158160394+devansh280904@users.noreply.github.com>
2024-02-29 20:14:59 +0530

    Add files via upload

---
+++ COA/dtb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void dtb(int n)
+{
+    int i=0,j,a[n];
+    while(n>0)
+    {
+        a[i]=n%2;
+        n=n/2;
+        i++;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        printf("%d",a[j]);
+    }
+}
+void main()
+{
+    int a;
+    printf("ENTER THE DECIMAL NUMBER: ");
+    scanf("%d",&a);
+    printf("BINARY NUMBER OF GIVEN DECIMAL NUMBER IS:");
+    dtb(a);
+}
