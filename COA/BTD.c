7efb4b6ee6bccba6d9ed93bc0fd4d91133489f12
devansh280904 <158160394+devansh280904@users.noreply.github.com>
2024-02-29 20:12:12 +0530

    BTD.c

--- COA/BTD.c
+++ COA/BTD.c
@@ -1,1 +1,18 @@
+#Implement a program for Binary to Decimal Conversion
 
+#include<stdio.h>
+void main()
+{
+  int dec=0,rem,n,bin,base=1;
+  printf("ENTER BINARY NO.:");
+  scanf("%d",&n);
+  bin=n;
+  while(n>0)
+  {
+    rem=n%10;
+    dec=dec+rem*base;
+    n=n/10;
+    base=base*2;
+  }
+  printf("DECIMAL NUMBER OF GIVEN BINARY NO. IS :%d",dec);
+}
