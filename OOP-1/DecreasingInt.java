a6276facbdab4590290a20fee8f3f3616455b04c
Devansh Kapadia <158160394+devansh280904@users.noreply.github.com>
2024-03-21 21:37:07 +0530

    Create DecreasingInt.java

---
+++ OOP-1/DecreasingInt.java
@@ -0,0 +1,31 @@
+//Importing Libraries
+import java.util.Scanner;
+
+// Main class
+public class Hannibal{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[]num= new int[10];
+
+        for(int i = 0 ; i < 10 ; i++){
+        System.out.println("Enter Integer "+(i+1) +":");
+        num[i]=sc.nextInt();
+        }
+        for(int i = 0 ; i < 10 ; i++){
+            for(int j = i+1 ; j< 10 ; j++){
+                if(num[i]<num[j]){
+                    int temp=num[i];
+                    num[i]=num[j];
+                    num[j]=temp;
+                }
+            }
+        }
+        System.out.println("Integers in Decreasing Order:");
+        for(int i = 0 ; i < 10 ; i++){
+            System.out.println(num[i]);
+        }
+      
+      // Clossing scanner 
+        sc.close();
+    }
+}
