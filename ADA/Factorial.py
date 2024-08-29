98fc0d022213a9ebe3acbdae949838d7da61ce52
Devansh Kapadia <158160394+devansh280904@users.noreply.github.com>
2024-08-29 19:19:44 +0530

    Update Factorial.py

--- ADA/Factorial.py
+++ ADA/Factorial.py
@@ -2,25 +2,27 @@
 
 import time
 
-start=time.time()
-
 # Iterative method
+start=time.time()
 def Fact(num):
     fact=1
     for i in range (1,num+1):
         fact=fact*i
     return fact
+end=time.time()
 
 # Recursive method
+start1=time.time()
 def Fact1(num):
     if num==0:
         return 1
     else:
         return num*Fact1(num-1)
+end1=time.time()
 
 num=int(input("Enter the Number:"))
-end=time.time()
 
 print("Factorial of Given Number in Iterative method:",Fact(num))
+print("Execution time in Iterative method:",end-start,"seconds")
 print("Factorial of Given Number in Recursive method:",Fact1(num))
-print("Execution time:",end-start,"seconds")
+print("Execution time in Recursive method:",end1-start1,"seconds")
