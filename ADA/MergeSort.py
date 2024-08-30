68c178ec54fb9c615f5044ff953eadb2c01fc5f4
Devansh Kapadia <158160394+devansh280904@users.noreply.github.com>
2024-08-30 09:38:47 +0530

    Create MergeSort.py

---
+++ ADA/MergeSort.py
@@ -0,0 +1,39 @@
+import time
+start=time.time()
+
+def MergeSort(arr):
+    if len(arr)<=1:
+        return arr
+    else:
+        mid=len(arr)//2
+        left=MergeSort(arr[:mid])
+        right=MergeSort(arr[mid:])
+        return Merge(left,right)
+
+def Merge(left,right):
+    sl=[]
+    i=0
+    j=0
+    while i < len(left) and  j < len(right):
+        if left[i] < right[j]:
+            sl.append(left[i])
+            i += 1
+        else: 
+            sl.append(right[j])
+            j += 1
+    sl.extend(left[i:])
+    sl.extend(right[j:])
+    return sl
+
+num=int(input("Enter size of array: "))
+arr=[]
+
+for i in range(num):
+    val=int(input("Enter the values: "))
+    arr.append(val)
+print("Unsorted Array:",arr)  
+res=MergeSort(arr)
+end=time.time()
+print("Sorted Array:",res)
+print("Execution time:",end-start,"seconds")
+
